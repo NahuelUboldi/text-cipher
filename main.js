10f1da493a90a8fa4becdeae45f7b181758020cb
NahuelUboldi <nahueluboldi@gmail.com>
2023-04-13 12:26:07 -0300

    working on ui

--- main.js
+++ main.js
@@ -1,49 +1,47 @@
 import cipher from './assets/js/cipher.js';
 import cipherKeys from './assets/js/keys.js';
 
-const inputText = document.querySelector('.user-text textarea');
-const resultText = document.querySelector('.result-text div');
 const encryptBtn = document.querySelector('.btn-encrypt');
 const decryptBtn = document.querySelector('.btn-decrypt');
-const copyTextBtnCode =
-  '<button class="btn btn-secondary btn-copy-text">Copiar</button>';
 
 function isValidText(text) {
   const onlyLowerAndCharacters = /[^a-z]/;
   return !onlyLowerAndCharacters.test(text);
 }
 
-function handleEncrypt() {
-  if (!isValidText(inputText.value)) {
-    alert('Solo se aceptan letras minúsculas y sin acentos');
-    return;
-  }
-  const encryptedText = cipher(inputText.value, cipherKeys, 'encrypt');
+function writeOutputText(encryptedText) {
+  const resultText = document.querySelector('.result-text div');
+  const copyTextBtnCode =
+    '<button class="btn btn-secondary btn-copy-text">Copiar</button>';
   resultText.innerHTML = `
   <span>${encryptedText}</span>  ${copyTextBtnCode}
   `;
   const copyTextBtn = document.querySelector('.btn-copy-text');
   copyTextBtn.addEventListener('click', handleCopy);
 }
-function handleDecrypt() {
+
+function handleClick(action) {
+  const inputText = document.querySelector('.user-text textarea');
   if (!isValidText(inputText.value)) {
     alert('Solo se aceptan letras minúsculas y sin acentos');
     return;
   }
-  const decryptedText = cipher(inputText.value, cipherKeys, 'decrypt');
-  resultText.innerHTML = `
-  <span>${decryptedText}</span>  ${copyTextBtnCode}
-  `;
-
-  const copyTextBtn = document.querySelector('.btn-copy-text');
-  copyTextBtn.addEventListener('click', handleCopy);
+  const encryptedText = cipher(inputText.value, cipherKeys, action);
+  writeOutputText(encryptedText);
 }
 
+function showCopyAlert() {
+  const alert = document.querySelector('.alert');
+  alert.classList.remove('hidden');
+  setTimeout(() => {
+    alert.classList.add('hidden');
+  }, 1500);
+}
 function handleCopy() {
   const textToCopy = document.querySelector('.result-text div span');
-  console.log(textToCopy.textContent);
   navigator.clipboard.writeText(textToCopy.textContent);
+  showCopyAlert();
 }
 
-encryptBtn.addEventListener('click', handleEncrypt);
-decryptBtn.addEventListener('click', handleDecrypt);
+encryptBtn.addEventListener('click', () => handleClick('encrypt'));
+decryptBtn.addEventListener('click', () => handleClick('decrypt'));
